c6f48bf13944ae9ae617575a289e0b766c7f563f
crackerjacques <129548912+crackerjacques@users.noreply.github.com>
2024-08-25 04:56:36 +0900

    Update PluginProcessor.cpp

--- lgpio_pwm/Source/PluginProcessor.cpp
+++ lgpio_pwm/Source/PluginProcessor.cpp
@@ -3,7 +3,13 @@
 
 PWMControlAudioProcessor::PWMControlAudioProcessor()
     : AudioProcessor (BusesProperties()
-                      .withOutput ("Output", juce::AudioChannelSet::stereo(), true)),
+                      #if ! JucePlugin_IsMidiEffect
+                       #if ! JucePlugin_IsSynth
+                        .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                       #endif
+                        .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                      #endif
+                    ),
     parameters (*this, nullptr, juce::Identifier ("PWMControl"),
         {
             std::make_unique<juce::AudioParameterChoice>("pwmChannel", "PWM Channel", juce::StringArray("Channel 0 (GPIO 18)", "Channel 1 (GPIO 19)"), 0),
@@ -40,15 +46,90 @@ const juce::String PWMControlAudioProcessor::getName() const
     return JucePlugin_Name;
 }
 
+bool PWMControlAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool PWMControlAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool PWMControlAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double PWMControlAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int PWMControlAudioProcessor::getNumPrograms()
+{
+    return 1;
+}
+
+int PWMControlAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void PWMControlAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String PWMControlAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void PWMControlAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
 void PWMControlAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    //nothing to do 
 }
 
 void PWMControlAudioProcessor::releaseResources()
 {
-    //nothing to do 
+
 }
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool PWMControlAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
 
 void PWMControlAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
 {
@@ -59,13 +140,13 @@ void PWMControlAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, j
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
+    // refresh
     static int updateCounter = 0;
     if (++updateCounter >= 100) {
         updatePWMState();
         updateCounter = 0;
     }
 
-    // audio passthru
     for (int channel = 0; channel < totalNumInputChannels; ++channel)
     {
         auto* channelData = buffer.getWritePointer (channel);
@@ -134,7 +215,7 @@ void PWMControlAudioProcessor::updatePWMState()
         float pwmDuty = *pwmDutyParameter;
         float pwmFrequency = static_cast<float>(*pwmFrequencyParameter);
 
-        // when pin changed
+        // when change pin, pwm will be stopped.
         int newPWMPin = pwmChannel == 0 ? 18 : 19;
         if (newPWMPin != currentPWMPin) {
             lgTxPwm(handle, currentPWMPin, 0, 0, 0, 0);
@@ -148,7 +229,7 @@ void PWMControlAudioProcessor::updatePWMState()
             currentPWMPin = newPWMPin;
         }
 
-
+        // PWM設定
         int result = lgTxPwm(handle, currentPWMPin, pwmFrequency, pwmDuty, 0, 0);
         if (result < 0) {
             juce::Logger::writeToLog("Failed to set PWM: " + juce::String(result));

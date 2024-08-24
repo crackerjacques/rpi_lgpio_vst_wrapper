cc31c318b300b3512a23b225f403140dea3cc5b4
crackerjacques <129548912+crackerjacques@users.noreply.github.com>
2024-08-25 03:45:44 +0900

    Update PluginEditor.h

--- lgpio_gpio/Source/PluginEditor.h
+++ lgpio_gpio/Source/PluginEditor.h
@@ -15,9 +15,12 @@ public:
 private:
     LgpioGpioAudioProcessor& audioProcessor;
     juce::AudioProcessorValueTreeState& valueTreeState;
+    const LgpioGpioAudioProcessor& processorRef;
 
     std::array<std::unique_ptr<juce::ToggleButton>, 28> gpioButtons;
     std::array<std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment>, 28> gpioAttachments;
+    std::array<std::unique_ptr<juce::ToggleButton>, 28> gpioModeButtons;
+    std::array<std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment>, 28> gpioModeAttachments;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (LgpioGpioAudioProcessorEditor)
 };
